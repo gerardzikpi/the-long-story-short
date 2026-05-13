6f1d87bd368821f576d88f764893b3ca45d51830
Gerard Zikpi <kodinapetra@yahoo.com>
2026-05-13 12:05:43 +1200

    updated the port variable

--- server.ts
+++ server.ts
@@ -30,7 +30,7 @@ async function initData() {
 async function startServer() {
   await initData();
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
